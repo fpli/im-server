a13d71220c327e4882e39472a613d280f6a991d0
fpli <1394220859@qq.com>
2019-08-14 13:06:26 +0800

    channel处理器

--- src/main/java/com/sap/mim/net/ChildNioSocketChannelHandler.java
+++ src/main/java/com/sap/mim/net/ChildNioSocketChannelHandler.java
@@ -2,19 +2,16 @@ package com.sap.mim.net;
 
 import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
 
 /**
  * 描述:客户端对应的Channel处理器
  */
-public class ChildNioSocketChannelHandler extends ChannelHandlerAdapter {
+public class ChildNioSocketChannelHandler extends SimpleChannelInboundHandler<SmartSIMProtocol> {
 
-    // 用于获取客户端发送的信息
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        // 用于获取客户端发来的数据信息
-        SmartSIMProtocol body = (SmartSIMProtocol) msg;
-        System.out.println("Server接受的客户端的信息 :" + body.toString());
-
+    protected void messageReceived(ChannelHandlerContext ctx, SmartSIMProtocol msg) throws Exception {
+        System.out.println("Server接受的客户端的信息 :" + msg.toString());
         // 会写数据给客户端
         String str = "Hi I am Server ...";
         SmartSIMProtocol response = new SmartSIMProtocol();
@@ -22,10 +19,6 @@ public class ChildNioSocketChannelHandler extends ChannelHandlerAdapter {
         response.setContent(str.getBytes());
         // 当服务端完成写操作后，关闭与客户端的连接
         ctx.writeAndFlush(response);
-        // .addListener(ChannelFutureListener.CLOSE);
-
-        // 当有写操作时，不需要手动释放msg的引用
-        // 当只有读操作时，才需要手动释放msg的引用
     }
 
     @Override
