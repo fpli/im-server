efdc6e92fe4bfd8b5e515538c27d3f89aaac7414
fpli <1394220859@qq.com>
2019-08-15 10:37:34 +0800

    channel处理器, 封装消息模型

--- src/main/java/com/sap/mim/net/ChildChannelInitializer.java
+++ src/main/java/com/sap/mim/net/ChildChannelInitializer.java
@@ -2,12 +2,14 @@ package com.sap.mim.net;
 
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.timeout.IdleStateHandler;
 
 public class ChildChannelInitializer extends ChannelInitializer<NioSocketChannel> {
 
     @Override
     protected void initChannel(NioSocketChannel ch) throws Exception {
         // 添加自定义协议的编解码工具
+        ch.pipeline().addLast(new IdleStateHandler(10,0,0));
         ch.pipeline().addLast(new SmartSIMEncoder());
         ch.pipeline().addLast(new SmartSIMDecoder());
         // 处理网络IO
