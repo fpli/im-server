f31dca1842fa60a8e7fe76acabf0f70facc69fd6
fpli <1394220859@qq.com>
2019-08-15 18:22:08 +0800

    channel处理器, 封装消息模型

--- src/main/java/com/sap/mim/server/EchoServer.java
+++ src/main/java/com/sap/mim/server/EchoServer.java
@@ -17,15 +17,10 @@ public class EchoServer {
     }
 
     public void start() throws Exception {
-        //指定 NioEventLoopGroup 来接受和处理新连接
         EventLoopGroup group = new NioEventLoopGroup();
         try {
-            //create ServerBootstrap instance
             ServerBootstrap b = new ServerBootstrap();
-            //Specifies NIO transport, local socket address
-            /*Adds handler to channel pipeline 指定通道类型为 NioServerSocketChannel
-            设置 InetSocketAddress 让服务器监听某个端口已等待客户端连接
-             */
+
             b.group(group).channel(NioServerSocketChannel.class).
                     localAddress(port).childHandler(new ChannelInitializer<NioSocketChannel>() {
                 @Override
@@ -34,11 +29,7 @@ public class EchoServer {
                     ch.pipeline().addLast(new EchoChildHandler());
                 }
             });
-            /*Binds server, waits for server to close, and releases resources
-             * 绑定服务器等待直到绑定完成，调用 sync()方法会阻塞直到服务器完成绑定，
-             * 然后服务器等待通道关闭，因为使用 sync()，所以关闭操作也会被阻塞。
-             * 现在你可以关闭EventLoopGroup 和释放所有资源，包括创建的线程。
-             * */
+
             ChannelFuture f = b.bind().sync();
             System.out.println(EchoServer.class.getName() + "started and listen on “" + f.channel().localAddress());
             f.channel().closeFuture().sync();
