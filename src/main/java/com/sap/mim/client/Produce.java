0f15ec52f0a7e662bfd4ea6207a0f66f05f5adfe
fpli <1394220859@qq.com>
2019-08-21 12:40:58 +0800

    channel处理器, 封装消息模型

---
+++ src/main/java/com/sap/mim/client/Produce.java
@@ -0,0 +1,74 @@
+package com.sap.mim.client;
+
+
+import com.sap.mim.bean.LoginMessage;
+import com.sap.mim.bean.MessageModel;
+import com.sap.mim.bean.MessageType;
+import com.sap.mim.net.ConstantValue;
+import com.sap.mim.net.SmartSIMProtocol;
+import com.sap.mim.util.MessageIdGenerator;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.Random;
+
+public class Produce {
+
+    private final String host;
+    private final int port;
+    private Channel channel;
+
+    public Produce(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public void run() throws InterruptedException, IOException {
+        EventLoopGroup group = new NioEventLoopGroup();
+
+        try {
+            Bootstrap bootstrap = new Bootstrap().group(group).channel(NioSocketChannel.class).handler(new ProduceInitializer());
+            channel = bootstrap.connect(host, port).sync().channel();
+            Random random = new Random();
+            while (true) {
+                LoginMessage loginMessage = new LoginMessage();
+                loginMessage.setMessageType(MessageType.C2S);
+                loginMessage.setMsgId(MessageIdGenerator.getMsgId());
+                loginMessage.setAccountNo("ggggggggggg");
+                loginMessage.setPassword("123456");
+                sendMessageModel(loginMessage);
+                System.out.println("消息发送--->"+123456);
+                Thread.currentThread().sleep(random.nextInt(10) * 1000);
+            }
+        } finally {
+            group.shutdownGracefully();
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        new Produce("localhost", 5000).run();
+    }
+
+    public void sendMessageModel(MessageModel messageModel){
+        try {
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            ObjectOutputStream objectOutputStream       = new ObjectOutputStream(byteArrayOutputStream);
+            objectOutputStream.writeObject(messageModel);
+            byte[] content = byteArrayOutputStream.toByteArray();
+            SmartSIMProtocol request = new SmartSIMProtocol();
+            request.setHead_data(ConstantValue.HEAD_DATA);
+            request.setContentLength(content.length);
+            request.setContent(content);
+            channel.writeAndFlush(request);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/sap/mim/client/ProduceInitializer.java
@@ -0,0 +1,21 @@
+package com.sap.mim.client;
+
+
+import com.sap.mim.net.SmartSIMDecoder;
+import com.sap.mim.net.SmartSIMEncoder;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.timeout.IdleStateHandler;
+
+public class ProduceInitializer extends ChannelInitializer<NioSocketChannel> {
+
+    protected void initChannel(NioSocketChannel ch) throws Exception {
+        ChannelPipeline pi = ch.pipeline();
+        pi.addLast(new IdleStateHandler(0,0,5));
+        // 添加自定义协议的编解码工具
+        pi.addLast(new SmartSIMEncoder());
+        pi.addLast(new SmartSIMDecoder());
+        pi.addLast("handler", new ClientBizInboundHandler());
+    }
+}
