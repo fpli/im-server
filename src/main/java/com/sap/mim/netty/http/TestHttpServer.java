6df13e8c7ebaaf8563de200d8a6cb756fb0e3625
fpli <1394220859@qq.com>
2020-04-19 18:44:10 +0800

    fix bug

--- src/main/java/com/sap/mim/netty/http/TestHttpServer.java
+++ src/main/java/com/sap/mim/netty/http/TestHttpServer.java
@@ -2,7 +2,6 @@ package com.sap.mim.netty.http;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 
