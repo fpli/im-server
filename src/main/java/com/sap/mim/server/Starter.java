2a05d6d890fab4a794b7faa030eccd651e688629
fpli <1394220859@qq.com>
2020-04-19 21:42:39 +0800

    fix bug

--- src/main/java/com/sap/mim/server/Starter.java
+++ src/main/java/com/sap/mim/server/Starter.java
@@ -14,7 +14,7 @@ public class Starter {
 
     public static void main(String[] args) throws Exception {
         // 配置NIO线程组
-        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
         EventLoopGroup workerGroup = new NioEventLoopGroup();
         try {
             // 服务器辅助启动类配置
